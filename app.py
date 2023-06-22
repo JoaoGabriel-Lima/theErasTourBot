ae7edcb1a6353681514ccf87b9e492266ae9f869
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2023-06-22 07:44:57 -0300

    update

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-# create a program that will ask the user how many windows they want to open and then open that many windows with webdrivers with diferent sessions
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-import time
-
+# local = "SP" # "SP" ou "RJ"
 number_of_windows = int(input("Quantas filas? "))
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_argument("--incognito --window-size=400,400")
 
 print("Não feche o programa até que seu ingresso seja comprado!")
+link = ""
 
 driver_list = []
 posx = 0
 posy = 0
 
+
 for i in range(number_of_windows):
     driver = webdriver.Chrome(options=options)
-    driver.get("https://taylor-swift-rj.sales.ticketsforfun.com.br/")
+    driver.get("https://www.taylorswifttheerastour.com.br/")
+    if link != "":
+        driver.get(link)
+    else:
+        driver.find_element(By.XPATH, "/html/body/div/section[2]/div/div[3]/a[1]").click()
     driver.set_window_position(posx,posy)
     if posx+470 > 1450:
         posx = 0
@@ -32,12 +35,13 @@ for i in range(number_of_windows):
 
 
     driver_list.append(driver)
-    # driver.find_element(By.XPATH, "/html/body/div/section[2]/div/div[3]/a[1]").click()
 
-    # get cookies after clicking on the button
-    print("--------------------")
-    print("ID DO NAVEGADOR "+str(i+1)+": " + driver.get_cookie("Queue-it")["value"])
-    print("--------------------")
+    #! COOKIES (Não apague o comentário abaixo)
+    if link != "":
+        driver.find_element(By.XPATH, "/html/body/div/section[2]/div/div[3]/a[1]").click()
+        print("--------------------")
+        print("ID DO NAVEGADOR "+str(i+1)+": " + driver.get_cookie("Queue-it")["value"])
+        print("--------------------")
 
 
     
